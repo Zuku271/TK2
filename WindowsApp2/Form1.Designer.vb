ebcf24c3e5583c225e8588ec70c48fe542f71058
Zuku271 <adrian.zuchowski@outlook.com>
2018-11-25 00:23:06 +0100

    Baza damych cd.

--- WindowsApp2/Form1.Designer.vb
+++ WindowsApp2/Form1.Designer.vb
@@ -32,13 +32,28 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
+        Me.LoginButton = New System.Windows.Forms.Button()
+        Me.LoginBox = New System.Windows.Forms.TextBox()
+        Me.PasswordBox = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.AddNewItemBox = New System.Windows.Forms.TextBox()
+        Me.AddNewItemButton = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'CarList
         '
         Me.CarList.FormattingEnabled = True
         Me.CarList.Items.AddRange(New Object() {"Audi", "Skoda", "Honda", "Porsche", "Chevrolet"})
-        Me.CarList.Location = New System.Drawing.Point(86, 129)
+        Me.CarList.Location = New System.Drawing.Point(45, 291)
         Me.CarList.Name = "CarList"
         Me.CarList.Size = New System.Drawing.Size(120, 95)
         Me.CarList.TabIndex = 0
@@ -46,7 +61,7 @@ Partial Class Form1
         'ModelList
         '
         Me.ModelList.FormattingEnabled = True
-        Me.ModelList.Location = New System.Drawing.Point(212, 129)
+        Me.ModelList.Location = New System.Drawing.Point(171, 291)
         Me.ModelList.Name = "ModelList"
         Me.ModelList.Size = New System.Drawing.Size(120, 95)
         Me.ModelList.TabIndex = 0
@@ -54,7 +69,7 @@ Partial Class Form1
         'ColorType
         '
         Me.ColorType.FormattingEnabled = True
-        Me.ColorType.Location = New System.Drawing.Point(464, 129)
+        Me.ColorType.Location = New System.Drawing.Point(423, 291)
         Me.ColorType.Name = "ColorType"
         Me.ColorType.Size = New System.Drawing.Size(120, 95)
         Me.ColorType.TabIndex = 0
@@ -62,7 +77,7 @@ Partial Class Form1
         'Color
         '
         Me.Color.FormattingEnabled = True
-        Me.Color.Location = New System.Drawing.Point(590, 129)
+        Me.Color.Location = New System.Drawing.Point(549, 291)
         Me.Color.Name = "Color"
         Me.Color.Size = New System.Drawing.Size(120, 95)
         Me.Color.TabIndex = 0
@@ -70,7 +85,7 @@ Partial Class Form1
         'EngineType
         '
         Me.EngineType.FormattingEnabled = True
-        Me.EngineType.Location = New System.Drawing.Point(338, 129)
+        Me.EngineType.Location = New System.Drawing.Point(297, 291)
         Me.EngineType.Name = "EngineType"
         Me.EngineType.Size = New System.Drawing.Size(120, 95)
         Me.EngineType.TabIndex = 0
@@ -78,7 +93,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(83, 103)
+        Me.Label1.Location = New System.Drawing.Point(42, 265)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(37, 13)
         Me.Label1.TabIndex = 1
@@ -87,7 +102,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(209, 103)
+        Me.Label2.Location = New System.Drawing.Point(168, 265)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(36, 13)
         Me.Label2.TabIndex = 1
@@ -96,7 +111,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(335, 103)
+        Me.Label3.Location = New System.Drawing.Point(294, 265)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(32, 13)
         Me.Label3.TabIndex = 1
@@ -105,7 +120,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(461, 103)
+        Me.Label4.Location = New System.Drawing.Point(420, 265)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(36, 13)
         Me.Label4.TabIndex = 1
@@ -114,17 +129,160 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(587, 103)
+        Me.Label5.Location = New System.Drawing.Point(546, 265)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(31, 13)
         Me.Label5.TabIndex = 1
         Me.Label5.Text = "Kolor"
         '
+        'LoginButton
+        '
+        Me.LoginButton.Location = New System.Drawing.Point(65, 119)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(75, 23)
+        Me.LoginButton.TabIndex = 2
+        Me.LoginButton.Text = "Logowanie"
+        Me.LoginButton.UseVisualStyleBackColor = True
+        '
+        'LoginBox
+        '
+        Me.LoginBox.Location = New System.Drawing.Point(65, 49)
+        Me.LoginBox.Name = "LoginBox"
+        Me.LoginBox.Size = New System.Drawing.Size(139, 20)
+        Me.LoginBox.TabIndex = 3
+        '
+        'PasswordBox
+        '
+        Me.PasswordBox.Location = New System.Drawing.Point(65, 80)
+        Me.PasswordBox.Name = "PasswordBox"
+        Me.PasswordBox.Size = New System.Drawing.Size(139, 20)
+        Me.PasswordBox.TabIndex = 3
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(23, 49)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(36, 13)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Login:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(23, 83)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Hasło:"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(226, 38)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(55, 17)
+        Me.RadioButton1.TabIndex = 6
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Marka"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(226, 61)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(54, 17)
+        Me.RadioButton2.TabIndex = 6
+        Me.RadioButton2.Text = "Model"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(226, 84)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(50, 17)
+        Me.RadioButton3.TabIndex = 6
+        Me.RadioButton3.Text = "Silnik"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(226, 107)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(89, 17)
+        Me.RadioButton4.TabIndex = 6
+        Me.RadioButton4.Text = "Wyposażenie"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(226, 130)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(54, 17)
+        Me.RadioButton5.TabIndex = 6
+        Me.RadioButton5.Text = "Lakier"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(226, 153)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(49, 17)
+        Me.RadioButton6.TabIndex = 6
+        Me.RadioButton6.Text = "Kolor"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'AddNewItemBox
+        '
+        Me.AddNewItemBox.Location = New System.Drawing.Point(10, 38)
+        Me.AddNewItemBox.Name = "AddNewItemBox"
+        Me.AddNewItemBox.Size = New System.Drawing.Size(139, 20)
+        Me.AddNewItemBox.TabIndex = 3
+        '
+        'AddNewItemButton
+        '
+        Me.AddNewItemButton.Location = New System.Drawing.Point(10, 70)
+        Me.AddNewItemButton.Name = "AddNewItemButton"
+        Me.AddNewItemButton.Size = New System.Drawing.Size(75, 23)
+        Me.AddNewItemButton.TabIndex = 7
+        Me.AddNewItemButton.Text = "Dodaj"
+        Me.AddNewItemButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.AddNewItemButton)
+        Me.GroupBox1.Controls.Add(Me.AddNewItemBox)
+        Me.GroupBox1.Controls.Add(Me.RadioButton6)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.RadioButton5)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Location = New System.Drawing.Point(600, 26)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(399, 222)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        Me.GroupBox1.Visible = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1022, 569)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.PasswordBox)
+        Me.Controls.Add(Me.LoginBox)
+        Me.Controls.Add(Me.LoginButton)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -137,6 +295,8 @@ Partial Class Form1
         Me.Controls.Add(Me.CarList)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -152,4 +312,18 @@ Partial Class Form1
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
+    Friend WithEvents LoginButton As Button
+    Friend WithEvents LoginBox As TextBox
+    Friend WithEvents PasswordBox As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents AddNewItemBox As TextBox
+    Friend WithEvents AddNewItemButton As Button
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
--- WindowsApp2/Form1.vb
+++ WindowsApp2/Form1.vb
@@ -1,7 +1,33 @@
-﻿Public Class Form1
-    Private Sub CarList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CarList.SelectedIndexChanged
-        ModelList.Items.Clear()
+﻿Imports System.Data
+Imports System.Data.Sql
+Imports System.Data.SqlClient
 
+Public Class Form1
+
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+
+        Dim cnString As String
+
+        Dim objTabela As DataTable
+        Dim objWiersz As DataRow
+        Dim dataset As DataSet
+        Dim dataadapter As SqlDataAdapter
+
+        Dim query_string As String
+        query_string = "SELECT * FROM Users WHERE Login = '" & LoginBox.Text & "' AND Password = '" & PasswordBox.Text & "';"
+        dataadapter = New SqlDataAdapter()
+        dataadapter.SelectCommand = New SqlCommand(query_string)
+        Dim sqlBuilder As SqlCommandBuilder
+
+        dataadapter = New SqlDataAdapter()
+        sqlBuilder = New SqlCommandBuilder(dataadapter)
+        'cnString = "Provider=Microsoft.Jet.OLEDB.4.0;Persist Security Info=False;Data Source=..\Debug\Database1.mdf"
+
+        'conn = New System.Data.SqlClient.SqlConnection(cnString)
+        'conn.Open()
+
+
+        'Dim objZapytanie As New System.Data.SqlClient.SqlConnection(strSQL, conn)
 
     End Sub
 End Class
